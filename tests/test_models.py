0d67fbc13e379408e101d167b2fce1cdd6b428c5
ubaumann <github@m.ubaumann.ch>
2022-07-26 09:36:28 +0200

    Add models test from dict, fix mypy

--- tests/test_models.py
+++ tests/test_models.py
@@ -19,8 +19,42 @@ def test_recursive_models() -> None:
                     ),
                 ],
             ),
-            Group(name="second second")
+            Group(name="second second"),
         ],
+    )
+    assert top.name == "Top"
+    assert top.repositories[0].name == "first level"
+    assert top.groups[0].name == "second level"
+    assert top.groups[1].name == "second second"
+    assert top.groups[0].groups[0].name == "third level"
+    assert len(top.groups[0].groups[0].repositories) == 3
+    assert top.groups[0].groups[0].repositories[2].name == "third level 3"
+
+
+def test_recursive_models_from_dict() -> None:
+    top = Group(
+        **{
+            "name": "Top",
+            "groups": [
+                {
+                    "name": "second level",
+                    "groups": [
+                        {
+                            "name": "third level",
+                            "groups": [],
+                            "repositories": [
+                                {"name": "third level 1"},
+                                {"name": "third level 2"},
+                                {"name": "third level 3"},
+                            ],
+                        }
+                    ],
+                    "repositories": [],
+                },
+                {"name": "second second", "groups": [], "repositories": []},
+            ],
+            "repositories": [{"name": "first level"}],
+        }
     )
     assert top.name == "Top"
     assert top.repositories[0].name == "first level"
