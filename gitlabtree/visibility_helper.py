0a45fe01e26710ffb845a19b00faf29f4e7a074b
Melchior Bodmer <melchior.bodmer@green.ch>
2022-07-28 14:21:13 +0200

    :sparkles: add visibility info

--- gitlabtree/visibility_helper.py
+++ gitlabtree/visibility_helper.py
@@ -1,8 +1,9 @@
 """
 Visibility Helper to get visibility information from GitLabAPI for projects and groups
 """
+from typing import Any, List
 
-from gitlabtree.gitlab_helper import GitLabHelper
+from gitlabtree.models import Group, Repository, VisibilityInfo
 
 
 class VisibilityHelper:
@@ -12,14 +13,40 @@ class VisibilityHelper:
 
     def __init__(self, gitlab) -> None:
         self.gitlab = gitlab
+        self.projects: List = []
+        self.groups: List = []
+        self.subgroups: List = []
 
-    def get_projects(self) -> None:
-        data = self.gitlab.get("projects")
-        for project in data:
-            id = project['id']
-            name = project['name']
-            namespace = project['namespace']['name']
-            visibility = project['visibility']
-            print(namespace + "/" + name)
-            print(visibility)
-        print(data)
+    def get_projects(self, group) -> Any:
+        """
+        get all projects for group
+        """
+        self.projects = self.gitlab.get(
+            f"groups/{group}/projects?include_subgroups=true"
+        )
+        return self.projects
+
+    def get_groups(self, group) -> Any:
+        """
+        get all groups
+        """
+        self.groups = self.gitlab.get(f"groups/{group}")
+        self.subgroups = self.gitlab.get(f"groups/{group}/descendant_groups")
+
+        top = Group(name=self.groups["name"])
+        _id_groups = {self.groups["id"]: top}
+        for group in sorted(self.subgroups, key=lambda i: i["full_path"]):
+            g = Group(
+                name=group["name"], info=[VisibilityInfo(text=group["visibility"])]
+            )
+            _id_groups[group["parent_id"]].groups.append(g)
+            _id_groups[group["id"]] = g
+
+        for project in self.projects:
+            p = Repository(
+                name=project["name"], info=[VisibilityInfo(text=project["visibility"])]
+            )
+            parent = project["namespace"]["id"]
+            _id_groups[parent].repositories.append(p)
+
+        return top
