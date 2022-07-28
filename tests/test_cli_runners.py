fc3ccbbab715e352948bfaf947371da73452832a
ubaumann <github@m.ubaumann.ch>
2022-07-29 00:18:33 +0200

    Fix cli runners tests

--- tests/test_cli_runners.py
+++ tests/test_cli_runners.py
@@ -7,19 +7,13 @@ runner = CliRunner()
 
 
 def test_help() -> None:
-    result = runner.invoke(app, ["runner", "--help"])
+    result = runner.invoke(app, ["runners", "--help"])
     assert result.exit_code == 0
-    assert "runner" in result.stdout
+    assert "runners" in result.stdout
     assert "Show the available runner" in result.stdout
 
 
-def test_wrong_command() -> None:
-    result = runner.invoke(app, ["pipeline", "nope"])
-    assert result.exit_code == 2
-    assert "Got unexpected extra argument (nope)" in result.output
-
-
 def test_no_command() -> None:
-    result = runner.invoke(app, ["pipeline"])
+    result = runner.invoke(app, ["runners"])
     assert result.exit_code == 2
     assert "Missing argument 'START'" in result.output
