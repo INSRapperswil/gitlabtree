5c650198f25ab5957efc2094382851ddfadd40ca
Melchior Bodmer <melchior.bodmer@green.ch>
2022-07-28 16:07:46 +0200

    :art: fix mypy issue

--- gitlabtree/visibility_helper.py
+++ gitlabtree/visibility_helper.py
@@ -1,9 +1,10 @@
 """
 Visibility Helper to get visibility information from GitLabAPI for projects and groups
 """
-from typing import Any, List
+from typing import Any, List, Dict
 
-from gitlabtree.models import Group, Repository, VisibilityInfo
+from .gitlab_helper import GitLabHelper
+from .models import Group, Repository, VisibilityInfo
 
 
 class VisibilityHelper:
@@ -11,13 +12,11 @@ class VisibilityHelper:
     Visibility helper to get visibility for projects and groups
     """
 
-    def __init__(self, gitlab) -> None:
+    def __init__(self, gitlab: GitLabHelper) -> None:
         self.gitlab = gitlab
-        self.projects: List = []
-        self.groups: List = []
-        self.subgroups: List = []
+        self.projects: List[Any] = []
 
-    def get_projects(self, group) -> Any:
+    def get_projects(self, group: str) -> Any:
         """
         get all projects for group
         """
@@ -26,27 +25,33 @@ class VisibilityHelper:
         )
         return self.projects
 
-    def get_groups(self, group) -> Any:
+    def get_groups(self, search_group: str) -> Group:
         """
         get all groups
         """
-        self.groups = self.gitlab.get(f"groups/{group}")
-        self.subgroups = self.gitlab.get(f"groups/{group}/descendant_groups")
+        groups: Dict[str, Any] = self.gitlab.get(f"groups/{search_group}")
+        subgroups: List[Dict[str, Any]] = self.gitlab.get(
+            f"groups/{search_group}/descendant_groups"
+        )
 
-        top = Group(name=self.groups["name"])
-        _id_groups = {self.groups["id"]: top}
-        for group in sorted(self.subgroups, key=lambda i: i["full_path"]):
-            g = Group(
+        top = Group(name=groups["name"])
+        _id_groups = {groups["id"]: top}
+        for group in sorted(subgroups, key=_get_full_path):
+            group_obj = Group(
                 name=group["name"], info=[VisibilityInfo(text=group["visibility"])]
             )
-            _id_groups[group["parent_id"]].groups.append(g)
-            _id_groups[group["id"]] = g
+            _id_groups[group["parent_id"]].groups.append(group_obj)
+            _id_groups[group["id"]] = group_obj
 
         for project in self.projects:
-            p = Repository(
+            project_obj = Repository(
                 name=project["name"], info=[VisibilityInfo(text=project["visibility"])]
             )
             parent = project["namespace"]["id"]
-            _id_groups[parent].repositories.append(p)
+            _id_groups[parent].repositories.append(project_obj)
 
         return top
+
+
+def _get_full_path(i: Dict[str, Any]) -> str:
+    return str(i["full_path"])
