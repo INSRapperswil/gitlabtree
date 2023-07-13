b764f039e11e56f3c5c07a59104267ffea18897a
ubaumann <github@m.ubaumann.ch>
2023-07-13 17:56:52 +0200

    Bump version to 0.1.3

--- gitlabtree/__init__.py
+++ gitlabtree/__init__.py
@@ -2,4 +2,4 @@
 GitLabTree
 """
 
-__version__ = "0.1.2"  # From Makefile
+__version__ = "0.1.3"  # From Makefile
--- tests/test_gitlabtree.py
+++ tests/test_gitlabtree.py
@@ -2,4 +2,4 @@ from gitlabtree import __version__
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.2"  # From Makefile
+    assert __version__ == "0.1.3"  # From Makefile
